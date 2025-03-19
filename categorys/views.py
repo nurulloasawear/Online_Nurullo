7b326c005a54bcb020c6b6e728de9e8bd470a273
nurulloasawear <nurullostepn3@gmail.com>
2025-03-19 19:04:16 +0500

    Forms & Auth

--- categorys/views.py
+++ categorys/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .forms import RegionForm
 # Create your views here.
+
+def Region(request):
+	form  =  RegionForm()
+	if request.method == 'POST':
+		form = RegionForm(request.POST)
+		print(request.POST)
+		print(form)
+		print(form.cleaned_data)
+		if form.is_valid():
+			form.save()
+			return redirect('region')
+		else:
+			form = RegionForm()
+	ctx = {
+		   'form':form
+		}
+	return render(request,'region.html',ctx)
+	
